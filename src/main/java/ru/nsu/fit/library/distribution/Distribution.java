dfc182c7706a05e26d4d7245415520b0a06df765
shaa <a.shishkin3@g.nsu.ru>
2021-05-07 10:55:24 +0700

    added distribution entity

---
+++ src/main/java/ru/nsu/fit/library/distribution/Distribution.java
@@ -0,0 +1,72 @@
+package ru.nsu.fit.library.distribution;
+
+import ru.nsu.fit.library.book.Book;
+import ru.nsu.fit.library.reader.Reader;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "distribution")
+public class Distribution {
+    @Id
+    @SequenceGenerator(name = "distribution_generator", sequenceName = "distribution_seq", initialValue = 60)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "distribution_generator")
+    @Column(name = "distribution_id")
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reader_id")
+    private Reader reader;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    @Column(name = "date_give")
+    private LocalDate dateGive;
+
+    @Column(name = "date_return")
+    private LocalDate dateReturn;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Reader getReader() {
+        return reader;
+    }
+
+    public void setReader(Reader reader) {
+        this.reader = reader;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public LocalDate getDateGive() {
+        return dateGive;
+    }
+
+    public void setDateGive(LocalDate dateGive) {
+        this.dateGive = dateGive;
+    }
+
+    public LocalDate getDateReturn() {
+        return dateReturn;
+    }
+
+    public void setDateReturn(LocalDate dateReturn) {
+        this.dateReturn = dateReturn;
+    }
+}
+
--- src/main/resources/db/migration/V1__CreateSchema.sql
+++ src/main/resources/db/migration/V1__CreateSchema.sql
@@ -16,8 +16,20 @@ CREATE TABLE books
 
 CREATE TABLE readers
 (
-    id           integer NOT NULL,
+    id                  integer NOT NULL,
     firstName           varchar(50) NOT NULL,
     lastName            varchar(50) NOT NULL,
     CONSTRAINT PK_reader PRIMARY KEY ( id )
+);
+
+CREATE TABLE distribution
+(
+    distribution_id integer NOT NULL,
+    reader_id       integer NOT NULL,
+    book_id         bigint NOT NULL,
+    date_give       date NOT NULL,
+    date_return     date,
+    CONSTRAINT PK_distribution PRIMARY KEY ( distribution_id ),
+    CONSTRAINT FK_reader FOREIGN KEY ( reader_id ) REFERENCES readers ( id ),
+    CONSTRAINT FK_edition FOREIGN KEY ( book_id ) REFERENCES books ( id )
 );
