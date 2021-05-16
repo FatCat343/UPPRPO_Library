df2542954333bbc2ac732966e67a5bed2b2fccce
iXtreme3 <i.nemtsev@g.nsu.ru>
2021-05-16 16:28:19 +0700

    created table for bookPosition

--- src/main/resources/db/migration/V1__CreateSchema.sql
+++ src/main/resources/db/migration/V1__CreateSchema.sql
@@ -10,8 +10,18 @@ CREATE TABLE books
     id bigint NOT NULL,
     title varchar(50) NOT NULL,
     author_id  bigint NOT NULL,
+    position_id bigint NOT NULL,
     CONSTRAINT PK_books PRIMARY KEY (id),
-    CONSTRAINT FK_books_authors FOREIGN KEY (author_id) REFERENCES authors
+    CONSTRAINT FK_books_authors FOREIGN KEY (author_id) REFERENCES authors,
+    CONSTRAINT FK_books_bookPosition FOREIGN KEY (position_id) REFERENCES bookPosition ( position_id )
+);
+
+CREATE TABLE bookPosition
+(
+    position_id  integer NOT NULL,
+    rack_number  integer NOT NULL,
+    shelf_number integer NOT NULL,
+    CONSTRAINT PK_bookPosition PRIMARY KEY ( position_id )
 );
 
 CREATE TABLE readers
